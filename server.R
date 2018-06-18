1f2fa523e4b6b96f231c4f1c76f0ea611ed6f137
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2018-06-18 23:15:03 +0200

    updated UI and filtering

--- server.R
+++ server.R
@@ -7,63 +7,80 @@ library(rlang)
 server <- function(input, output) {
   sample1 <- read.csv("https://raw.githubusercontent.com/michbur/shiny-density/master/data/sample1.csv")
   
-  
   output[["table"]] <- renderDataTable({
-    datatable(sample1)})
+    datatable(sample1)
+  })
   
   output[["text_density_plot"]] <- renderText({
-    paste("Density plot for", input$variable)})
+    paste("Density plot for", 
+          input[["variable"]])
+  })
   
-  ### sequences min/max ###
+  output[["density_plot"]] <- renderPlot({
+    ggplot(sample1, 
+           aes(x = sample1[, input[["variable"]]])) + 
+           labs(x = "value", y = "density") + 
+           geom_density() + 
+           coord_flip() + 
+           geom_vline(xintercept = coord[["y"]])
+    })
   
-  observeEvent(input$do, {
-    colmn <- input$column
-    n <- input$x
-    colmn_quo = parse_quosure(colmn)
-    minmax <- as.numeric(input$type)
-    #    print(colmn)
-    #    print(n)
-    #    print(input$column)
-    seq_column <- (select(sample1, c("seq", colmn)))
-    #    print(seq_column)
-    if (input$type==1){
+  ### density plot click ###
+  
+  coord <- reactiveValues(x = 0, y = 0)
+  coord[["y"]] <- 0
+  observeEvent(input[["plot_click"]], { 
+    coord[["y"]] <- input[["plot_click"]][["y"]]
+    coord[["x"]] <- input[["plot_click"]][["x"]]
+    })
+  
+  output[["test"]] <- renderText({
+    paste0("Threshold position: ", 
+           as.numeric(coord[["y"]]))
+    })   
+  
+  ### filtering ###
+  
+  observeEvent(input[["filter"]], {
+    if (is.null(input[["method"]])) { 
+      showModal(modalDialog(
+        title = "Error:",
+        "Please select the method of filtering.",
+        easyClose = TRUE))
+    } else {
+      colmn <- input[["variable"]]
+      colmn_quo = parse_quosure(colmn)
+      seq_column <- (select(sample1, c(seq, !!colmn_quo)))
       seq_sorted <- arrange(seq_column, !!colmn_quo)
-    } else if (input$type==2){
-      seq_sorted <- arrange(seq_column, desc(!!colmn_quo))
-    }
-    #    print(seq_sorted)
-    
-    output[["filtered_sequences"]] <- renderDataTable(
-      head(seq_sorted, n)
-    )
-  }
-  )
-  
-  
-  ### density plot ###
-  
-  coord <- reactiveValues(x=0, y=0)
-  
-  observeEvent(input$plot_click,  {
-    coord$y <- input$plot_click$y
-    coord$x <- input$plot_click$x
-    colm <- (parse_quosure(input$variable))
-    seq_c <- select(sample1, c(seq, !!colm))
-    seq_a <- arrange(seq_c, !!colm)
-    if(input$thresh_button==1){
-      sec_f <- filter(seq_a, !!colm<coord$y)
-      output[["table"]] <- renderDataTable({datatable(sec_f)})
-    }else if (input$thresh_button==2){
-      sec_f <- filter(seq_a, !!colm>coord$y)
-      output[["table"]] <- renderDataTable({datatable(sec_f)})
+      colnames(seq_sorted) <- c("sequence", "values")
+      
+      if (input[["method"]] == "Density plot") { 
+        if (input[["thresh_button"]] == 1) {
+          seq_out <- filter(seq_sorted, 
+                            values < coord[["y"]])
+        } else if (input[["thresh_button"]] == 2) {
+          seq_out <- filter(seq_sorted, 
+                            values > coord[["y"]])
+        }
+        
+      } else if (input[["method"]] == "Minimum/maximum values") {
+        n <- input[["x"]]
+        if (input[["type"]] == 1) {
+          seq_out <- head(seq_sorted, n)
+        } else if (input[["type"]] == 2) {
+          seq_out <- head(arrange(seq_sorted, desc(values)), n)
+        }
+      }
+      
+      output[["table"]] <- renderDataTable({
+        datatable(seq_out)
+        })
     }
   })
   
-  output[["test"]] <- renderText({
-    paste0("Filter position: ", as.numeric(coord$y))})
-  
-  output[["density_plot"]] <- renderPlot({ggplot(sample1, aes(x=sample1[, input$variable])) + labs(x="value", y="density") + 
-      geom_density() + coord_flip() + geom_vline(xintercept=coord$y)})
-  
-  
+  observeEvent(input[["reset"]], {
+    output[["table"]] <- renderDataTable({
+      datatable(sample1)
+      })
+  })
 }
--- ui.R
+++ ui.R
@@ -1,44 +1,41 @@
-ui <-  (navbarPage("Data Table", 
-                   
-  ### density plot panel ###             
-                   
-  tabPanel(("Density plot"),
-    sidebarPanel(
-      selectInput("variable", 
-                  label="Select a variable:", 
-                  choices = c("ben_WT", "ben_mal", "mal_WT"), 
-                  selected = "ben_WT"), 
-      br(),
-      textOutput("text_density_plot"),
-      plotOutput("density_plot", 
-                 click="plot_click"),
-      textOutput("test"),
-      br(),
-      radioButtons("thresh_button", 
-                    "Select values lower or higher than threshold?",
-                    choices=list("lower"=1, "higher"=2)), 
-                    selected="lower"),
+ui <-  (fluidPage(
+  
+  sidebarPanel(
+    radioButtons("method",
+                 label = "Method of filtering:",
+                 choices = c("Density plot", "Minimum/maximum values"), 
+                 selected = character(0)),
+    selectInput("variable", 
+                label = "Select a variable:", 
+                choices = c("ben_WT", "ben_mal", "mal_WT")), 
+    br(),
+    textOutput("text_density_plot"),
+    plotOutput("density_plot", 
+               click = "plot_click"),
+    textOutput("test"),
+    br(),
     
+    conditionalPanel(
+      condition = "input.method == 'Density plot'",
+      radioButtons("thresh_button", 
+                   "Select values lower or higher than threshold?",
+                   choices = list("Lower" = 1, "Higher" = 2))),
+    
+    conditionalPanel(
+      condition = "input.method == 'Minimum/maximum values'",  
+      radioButtons("type",
+                   "Select the minimum or maximum values?",
+                   choices = list("Minimum" = 1, "Maximum" = 2)),
+      numericInput("x",
+                   label = "Number of sequences to show:",
+                   value = 0,
+                   min = 0,
+                   max = 100)),
+    actionButton('filter',
+                 "Filter"),
+    actionButton("reset",
+                 "Reset")),
+  
   mainPanel(
-    dataTableOutput("table"))),
-                   
-  ### sequences with min/max values panel ##
-                   
-  tabPanel(("Min/Max values"),
-     sidebarPanel(   
-        numericInput("x",
-                    label="Select a number of sequences to show:",
-                    value=0,
-                    min=0,
-                    max=100),      
-        radioButtons("column",
-                      label ="Variable:",
-                      choices = list("ben_WT", "ben_mal", "mal_WT")),
-        radioButtons("type",
-                      label="Values:",
-                      choices = list("Minimum"=1, "Maximum"=2)),
-        actionButton("do", "Show")),
-                            
-     mainPanel(dataTableOutput("filtered_sequences")
-                            ))
+    dataTableOutput("table"))
 ))
