acff13ca0942a1a23c47562b3044c2fe6ad332af
michbur <michalburdukiewicz@gmail.com>
2018-08-29 22:59:53 +0200

    improved gene download

--- functions/process_data.R
+++ functions/process_data.R
@@ -8,15 +8,16 @@ library(XML)
 if(Sys.info()[["nodename"]] == "amyloid")
   sample1 <- read.csv("/home/michal/Dropbox/PepArray_results/2018-06-07/full_best_res.csv")
 
-gene_names_list <- pblapply(unique(sample1[["prot_id"]]), function(ith_name) try({
+gene_names_list <- pblapply(unique(sample1[["prot_id"]]), function(ith_name) {
   prot_ids <- entrez_search(db = "protein", term = ith_name)
   gene_ids <- entrez_link(dbfrom = "protein", id = prot_ids[["ids"]], db = "gene")
   genes <- entrez_fetch(db = "gene", id = gene_ids[["links"]][["protein_gene"]], rettype = "fasta")
+
+  gene_lines <- strsplit(genes, "\n")[[1]]
   
-  gene_list <- xmlToList(genes)
   data.frame(prot_id = ith_name,
-             gene_name = gene_list[["div"]][["div"]][["div"]][["dl"]][["dd"]][["text"]],
-             gene_address = paste0("https://www.ncbi.nlm.nih.gov", 
-                                   gene_list[["div"]][["div"]][["p"]][["a"]][[".attrs"]][["href"]]),
+             gene_name = strsplit(gene_lines[2], " ")[[1]][2],
+             gene_address = paste0("https://www.ncbi.nlm.nih.gov/", 
+                                   gsub("ID: ", "", gene_lines[grep(pattern = "ID: ", x = gene_lines)])),
              stringsAsFactors = FALSE)
-}, silent = TRUE)) 
+}) 
