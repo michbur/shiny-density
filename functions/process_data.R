9abdc0071490a7c97de37c5114205a0daaa4b96a
michbur <michalburdukiewicz@gmail.com>
2018-08-30 17:53:52 +0200

    processing xmls :(

--- functions/process_data.R
+++ functions/process_data.R
@@ -3,21 +3,27 @@ library(pbapply)
 library(dplyr)
 library(XML)
 
+# required to properly download xml from ncbi
+library(httr)
+config(http_version = 2)
+
 # functions to process and annotate data after BEST
 
 if(Sys.info()[["nodename"]] == "amyloid")
   sample1 <- read.csv("/home/michal/Dropbox/PepArray_results/2018-06-07/full_best_res.csv")
 
-gene_names_list <- pblapply(unique(sample1[["prot_id"]]), function(ith_name) {
-  prot_ids <- entrez_search(db = "protein", term = ith_name)
-  gene_ids <- entrez_link(dbfrom = "protein", id = prot_ids[["ids"]], db = "gene")
-  genes <- entrez_fetch(db = "gene", id = gene_ids[["links"]][["protein_gene"]], rettype = "fasta")
+gene_names_list <- pblapply(unique(sample1[["prot_id"]])[1L:3], function(ith_name) {
 
-  gene_lines <- strsplit(genes, "\n")[[1]]
-  
+  prot_ids <- entrez_search(db = "protein", term = ith_name)
+  config(http_version = 2)
+  gene_ids <- entrez_link(dbfrom = "protein", id = prot_ids[["ids"]], db = "gene")
+  config(http_version = 0)
+  genes <- entrez_fetch(db = "gene", id = gene_ids[["links"]][["protein_gene"]], rettype = "xml")
+
+  gene_list <- xmlToList(genes)
   data.frame(prot_id = ith_name,
-             gene_name = strsplit(gene_lines[2], " ")[[1]][2],
-             gene_address = paste0("https://www.ncbi.nlm.nih.gov/", 
-                                   gsub("ID: ", "", gene_lines[grep(pattern = "ID: ", x = gene_lines)])),
+             gene_name = gene_list[["Entrezgene"]][["Entrezgene_gene"]][["Gene-ref"]][["Gene-ref_locus"]],
+             gene_address = paste0("https://www.ncbi.nlm.nih.gov", 
+                                   gene_list[["Entrezgene"]][["Entrezgene_track-info"]][["Gene-track"]][["Gene-track_geneid"]]),
              stringsAsFactors = FALSE)
-}) 
+})
